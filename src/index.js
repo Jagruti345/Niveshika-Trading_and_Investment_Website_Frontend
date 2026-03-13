498567a5186cf39e252a0419829a21440baa3b9a
Jagruti Patil <patiljagruti658@gmail.com>
2026-03-13 18:09:47 +0530

    Implement routing and layout with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <div>
-      <h1>Hello</h1>
-    </div>
-  </React.StrictMode>
+  BrowserRouter>
+  <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/product" element={<ProductPage />} />
+      <Route path="/pricing" element={<Pricing />} />
+      <Route path="/support" element={<SupportPage />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+    <Footer />
+  </BrowserRouter>
 );
